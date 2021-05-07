c37a224a6bcbb60509b08d212437b804e292f1c6
GNemitz <Gwen.Nemitz@nelnet.net>
2021-05-07 10:01:05 -0500

    Commit Day 10 - working TS

--- Sprint5/Exercise2.ts
+++ Sprint5/Exercise2.ts
@@ -1,21 +1,24 @@
+export {};
+
 async function getWeather() {
   // first build the API call string by starting with the URL
-  var apiString = "https://api.weather.gov/gridpoints/";
+  let apiString = "https://api.weather.gov/gridpoints/";
   // next add the parameters to the string using the drop down lists
-  var theLocation = document.getElementById("location").value;
+  let theLocation: string = (<HTMLInputElement>document.getElementById("location")).value;
   apiString = apiString + theLocation + "/47,30/forecast";
-  //alert(apiString);  // show the API string
+  alert(apiString);  // show the API string
 
   // now make the API call to the web service using the string and store what is returned in response
   // response is data we got back
-  var response = await fetch(apiString);
+  let response: any = await fetch(apiString);
+  //var response = await fetch(apiString);
 
   // now, check the status property of the response object, 200-299 is valid
   if (response.status >= 200 && response.status <= 299) {  // valid status
     // read the response as JSON since it is a JSON file
-    var jsonData = await response.json();  
+    let jsonData: any = await response.json();  
     
-    days = jsonData.properties.periods.length;
+    let days: number = jsonData.properties.periods.length;
 
     document.getElementById("myDays").innerHTML = "";   // clear what was previously shown
     document.getElementById("myWeather").innerHTML = "";   // clear what was previously shown
