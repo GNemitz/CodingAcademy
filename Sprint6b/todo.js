2d582035852fccfbfc4675db5a4e98c34cf11687
GNemitz <Gwen.Nemitz@nelnet.net>
2021-05-11 13:16:38 -0500

    Day 12 - to do list working

--- Sprint6b/todo.js
+++ Sprint6b/todo.js
@@ -1,4 +1,4 @@
-//selectors
+//selectors - create objects - properties can change but object can't or can't be const
 var todoInput = document.querySelector('.todo_input');
 var todoButton = document.querySelector('.todo_button');
 var todoList = document.querySelector('.todo_list');
@@ -9,15 +9,15 @@ todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("click", filterTodo);
 //functions
 function addTodo(event) {
-    event.preventDefault();
+    event.preventDefault(); //preventdefault method of the event - cancels the event - default action of event will not occur - prevent from submitting a form?
     //todo DIV
     var todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
-    //todo LI 
-    var newTodo = document.createElement('li');
+    //todo LI within div
+    var newTodo = document.createElement('li'); //creating list items
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo_item');
-    todoDiv.appendChild(newTodo);
+    todoDiv.appendChild(newTodo); //adding list item to tododiv
     if (todoInput.value === "") {
         return null;
     }
@@ -30,41 +30,40 @@ function addTodo(event) {
     var deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
     deleteButton.classList.add('delete_btn');
-    todoDiv.appendChild(deleteButton);
+    todoDiv.appendChild(deleteButton); //appending delete button
     //Append to Actual LIST (along with the complete and trash buttons)
-    todoList.appendChild(todoDiv);
+    todoList.appendChild(todoDiv); //adding div to the page - list item and buttons
     //Clear todo input VALUE
     todoInput.value = "";
 }
 //DELETE & CHECK
 function deleteCheck(e) {
-    var item = e.target;
+    var item = e.target; //button that was clicked
+    var todo = item.parentElement; //todo is div - parent element is div 
     //DELETE ITEM
-    if (item.classList[0] === "delete_btn") {
-        var todo_1 = item.parentElement;
+    if (item.classList[0] === "delete_btn") { //list of classes that we added - element zero of classlist 
         //ANIMATION TRANSITION
-        todo_1.classList.add("fall");
-        todo_1.addEventListener('transitionend', function () {
-            todo_1.remove();
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function () {
+            todo.remove(); //deletes/removes todo which was div
         });
     }
     //COMPLETE ITEM
     if (item.classList[0] === "complete_btn") {
-        var todo = item.parentElement;
-        todo.classList.toggle("completedItem");
+        //const todo = item.parentElement;
+        todo.classList.toggle("completedItem"); //toggles - add or remove class
     }
 }
 //FILTERING THE TASKS ACCORDING THE OPTION
 function filterTodo(e) {
-    //let todos: string[] = todoList.childNodes;
-    var todos = todoList.childNodes;
-    for (var i = 1; i < todos.length; i++) {
-        switch (e.target.value) {
+    var todos = todoList.childNodes; //list of all LI items
+    for (var i = 0; i < todos.length; i++) { //loop through list
+        switch (e.target.value) { //switch is nested if  - target is click event
             case "all":
-                todos[i].style.display = "flex";
-                break;
+                todos[i].style.display = "flex"; //change style css to flex - show all
+                break; //get out
             case "completed":
-                if (todos[i].classList.contains('completedItem')) {
+                if (todos[i].classList.contains('completedItem')) { // change display to flex or none (don't display)
                     todos[i].style.display = "flex";
                 }
                 else {
@@ -72,11 +71,11 @@ function filterTodo(e) {
                 }
                 break;
             case "uncompleted":
-                if (!todos[i].classList.contains('completedItem')) {
+                if (!todos[i].classList.contains('completedItem')) { // if not completed, then uncompleted - change display
                     todos[i].style.display = "flex";
                 }
                 else {
-                    todos[i].style.display = "none";
+                    todos[i].style.display = "none"; //won't show
                 }
                 break;
         }
--- Sprint6b/todo.ts
+++ Sprint6b/todo.ts
@@ -1,24 +1,23 @@
-//selectors
-const todoInput = (<HTMLInputElement>document.querySelector('.todo_input'));
-const todoButton = (<HTMLInputElement>document.querySelector('.todo_button'));
-const todoList = (<HTMLInputElement>document.querySelector('.todo_list'));
-const filterOption = (<HTMLInputElement>document.querySelector('.filter_todo'));
+//selectors - create objects - properties can change but object can't or can't be const
+const todoInput: HTMLInputElement = (<HTMLInputElement>document.querySelector('.todo_input'));
+const todoButton: HTMLButtonElement = (<HTMLButtonElement>document.querySelector('.todo_button'));
+const todoList: HTMLUListElement = (<HTMLUListElement>document.querySelector('.todo_list'));
+const filterOption: HTMLSelectElement = (<HTMLSelectElement>document.querySelector('.filter_todo'));
 //event listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("click", filterTodo);   
 //functions
-
-function addTodo(event) {
-    event.preventDefault();
+function addTodo(event: Event) {  //passing in event object when button event is clicked
+    event.preventDefault();  //preventdefault method of the event - cancels the event - default action of event will not occur - prevent from submitting a form?
     //todo DIV
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo');
-    //todo LI 
-    const newTodo = document.createElement('li');
+    //todo LI within div
+    const newTodo = document.createElement('li');  //creating list items
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo_item');
-    todoDiv.appendChild(newTodo);
+    todoDiv.appendChild(newTodo);      //adding list item to tododiv
     if(todoInput.value === ""){
         return null
     }
@@ -31,52 +30,51 @@ function addTodo(event) {
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
     deleteButton.classList.add('delete_btn')
-    todoDiv.appendChild(deleteButton);
+    todoDiv.appendChild(deleteButton);   //appending delete button
     //Append to Actual LIST (along with the complete and trash buttons)
-    todoList.appendChild(todoDiv);
+    todoList.appendChild(todoDiv);  //adding div to the page - list item and buttons
     //Clear todo input VALUE
     todoInput.value = ""
 }
 
 //DELETE & CHECK
-function deleteCheck(e) {
-    const item = e.target;
+function deleteCheck(e: Event) {   //e is the event when button click
+    const item: HTMLButtonElement = <HTMLButtonElement>e.target;   //button that was clicked
+    const todo: HTMLDivElement = <HTMLDivElement>item.parentElement;  //todo is div - parent element is div 
     //DELETE ITEM
-    if (item.classList[0] === "delete_btn") {
-        const todo = item.parentElement;
+    if (item.classList[0] === "delete_btn") {  //list of classes that we added - element zero of classlist 
         //ANIMATION TRANSITION
         todo.classList.add("fall")
-        todo.addEventListener('transitionend', function () {
-            todo.remove()
+        todo.addEventListener('transitionend', function () {   //???
+            todo.remove()                 //deletes/removes todo which was div
         })
     }
     //COMPLETE ITEM
     if (item.classList[0] === "complete_btn") {
-        const todo = item.parentElement;
-        todo.classList.toggle("completedItem")
+        //const todo = item.parentElement;
+        todo.classList.toggle("completedItem")      //toggles - add or remove class
     }
 }
 //FILTERING THE TASKS ACCORDING THE OPTION
-function filterTodo(e) {
-    //let todos: string[] = todoList.childNodes;
-    const todos = todoList.childNodes;
-    for(let i = 1; i<todos.length; i++ ){
-        switch (e.target.value) {
+function filterTodo(e: Event) {
+    const todos = todoList.childNodes;          //list of all LI items
+    for(let i = 0; i<todos.length; i++ ){       //loop through list
+        switch (e.target.value) {               //switch is nested if  - target is click event
             case "all":
-                todos[i].style.display = "flex";
-                break;
+                todos[i].style.display = "flex";       //change style css to flex - show all
+                break;  //get out
             case "completed":
-                if (todos[i].classList.contains('completedItem')) {
+                if (todos[i].classList.contains('completedItem')) {  // change display to flex or none (don't display)
                     todos[i].style.display = "flex";
                 } else {
                     todos[i].style.display = "none";
                 }
                 break;
             case "uncompleted":
-                if (!todos[i].classList.contains('completedItem')) {
+                if (!todos[i].classList.contains('completedItem')) { // if not completed, then uncompleted - change display
                     todos[i].style.display = "flex";
                 } else {
-                    todos[i].style.display = "none";
+                    todos[i].style.display = "none";  //won't show
                 }
                 break;
         }
