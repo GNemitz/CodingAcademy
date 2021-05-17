e0ba91a250466ec996550dab5a974701329cd172
GNemitz <Gwen.Nemitz@nelnet.net>
2021-05-17 09:48:26 -0500

    MemoryGame - timer working

--- MemoryGame/memorygametimer.js
+++ MemoryGame/memorygametimer.js
@@ -67,24 +67,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     ];
     cardArray.sort(function () { return 0.5 - Math.random(); }); //randomize the card array
-    var minutesLabel = document.getElementById("minutes");
-    var secondsLabel = document.getElementById("seconds");
-    var totalSeconds = 0;
-    setInterval(setTime, 1000);
-    function setTime() {
-        ++totalSeconds;
-        secondsLabel.innerHTML = pad(totalSeconds % 60);
-        minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-    }
-    function pad(val) {
-        var valString = val + "";
-        if (valString.length < 2) {
-            return "0" + valString;
-        }
-        else {
-            return valString;
-        }
-    }
     var grid = document.querySelector('.grid');
     var resultDisplay = document.querySelector('#result'); //need #???
     var cardsChosen = []; //create empty array of cards chosen
@@ -92,6 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var cardsWon = [];
     //create your board
     function createBoard() {
+        Clock.start;
         for (var i = 0; i < cardArray.length; i++) {
             var card = document.createElement('img');
             card.setAttribute('src', 'images/blank.png');
@@ -128,6 +111,7 @@ document.addEventListener('DOMContentLoaded', function () {
         cardChosenId = [];
         if (cardsWon.length === cardArray.length / 2) {
             var winnerlabel = document.getElementById("winner");
+            Clock.pause;
             winnerlabel.innerHTML = "Congratulations!  You matched all the cards!";
         }
     }
@@ -141,6 +125,44 @@ document.addEventListener('DOMContentLoaded', function () {
             setTimeout(checkForMatch, 500); //buffer time before checking for match
         }
     }
+    //timer
+    var Clock = {
+        totalSeconds: 0,
+        start: function () {
+            if (!this.interval) {
+                var self = this;
+                function pad(val) { return val > 9 ? val : "0" + val; }
+                this.interval = setInterval(function () {
+                    self.totalSeconds += 1;
+                    document.getElementById("min").innerHTML = pad(Math.floor(self.totalSeconds / 60 % 60));
+                    document.getElementById("sec").innerHTML = pad(parseInt(self.totalSeconds % 60));
+                }, 1000);
+            }
+        },
+        reset: function () {
+            Clock.totalSeconds = null;
+            clearInterval(this.interval);
+            document.getElementById("min").innerHTML = "00";
+            document.getElementById("sec").innerHTML = "00";
+            delete this.interval;
+        },
+        pause: function () {
+            clearInterval(this.interval);
+            delete this.interval;
+        },
+        resume: function () {
+            this.start();
+        },
+        restart: function () {
+            this.reset();
+            Clock.start();
+        }
+    };
+    document.getElementById("startButton").addEventListener("click", function () { Clock.start(); });
+    document.getElementById("pauseButton").addEventListener("click", function () { Clock.pause(); });
+    document.getElementById("resumeButton").addEventListener("click", function () { Clock.resume(); });
+    document.getElementById("resetButton").addEventListener("click", function () { Clock.reset(); });
+    document.getElementById("restartButton").addEventListener("click", function () { Clock.restart(); });
     createBoard();
 });
 function reset() {
