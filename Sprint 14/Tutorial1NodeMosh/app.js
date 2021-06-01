69f547a6046b10bfc8d23ff70d86eba7b6b7f81c
GNemitz <Gwen.Nemitz@nelnet.net>
2021-06-01 14:32:54 -0500

    end of tutorial 1

--- Sprint 14/Tutorial1NodeMosh/app.js
+++ Sprint 14/Tutorial1NodeMosh/app.js
@@ -25,7 +25,78 @@ window.setTimeout
 //console.log(module);   //not global object
 //load module - require function with 1 argument
 //best to use const so it doesn't get overwriten - tools like jshint will scan JS code for errors like this
-const log = require('./logger');   //or logger.js node knows it is a js file   ./logger in same folder or  ../ if in parent folder
+//const log = require('./logger');   //or logger.js node knows it is a js file   ./logger in same folder or  ../ if in parent folder
 
 //console.log(logger);
-log('message');     
+//log('message');     
+
+//built in path module  (find in nodejs.org docs section)
+/*const path = require('path');   
+
+var pathObj = path.parse(__filename);             //parse method  __filename is argument
+
+console.log(pathObj); */
+
+//built in OS module - operating system
+/*const os = require('os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+//console.log('Total Memory: ' + totalMemory);
+
+//template string  - helps to build a string without concatenation
+//ES6 / ES2015  : ECMAScript 6
+
+console.log(`Total Memory: ${totalMemory}`);  //${} place holder for argument (dynamic)
+console.log(`Free Memory: ${freeMemory}`);
+
+//files in node
+const fs = require('fs');
+
+// directory method - display all files and folder in the current folder
+//const files = fs.readdirSync('./');   //always use async/non blocking (to not keep server busy - single thread) not synchronous
+//console.log(files);  //display string array
+
+fs.readdir('./', function(err, files) {  //first parameter is path, 2nd parameter is callback function
+  if (err) console.log('Error', err);  
+  else console.log('Results', files);  
+});
+
+
+const EventEmitter = require('events');   //EventEmitter is class/container/blue print therefore capitalize
+//const { emit } = require('process');   // I don't know where this came from???
+//const emitter = new EventEmitter();  //emitter is object that we can use
+
+
+const Logger = require('./logger');  //now class
+const logger = new Logger();    // instance of the custom class that you created that extends the EventEmitter - use below
+
+//register a listener - called when messageLogged event/function is raised
+//order is important - need to register first before raising event
+//arrow function - remove the word function
+logger.on('messageLogged', (arg) => {  //2 arguments - name of event, callback function (listener) - can pass arg - e, eventArg, arg,etc
+  console.log('Listener called', arg);
+});
+
+logger.log('message');
+*/
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('HelloWorld');
+    res.end();
+  }
+
+  if (req.url === '/api/courses') {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
+});    //is an event emitter
+
+server.listen(3000);
+
+console.log('Listening on port 3000...');
+
--- Sprint 14/Tutorial1NodeMosh/logger.js
+++ Sprint 14/Tutorial1NodeMosh/logger.js
@@ -1,19 +1,32 @@
 ///all wrapped in side of a function in node
 //(function (exports, require, module, __filename, __dirname) {     // Module wrapper function
 
+const EventEmitter = require('events');   //EventEmitter is class/container/blue print therefore capitalize
 
-  console.log(__filename);
-  console.log(__dirname);
+//  console.log(__filename);
+//  console.log(__dirname);
 
-  var url = 'http://mylogger.io/log';   //private
+var url = 'http://mylogger.io/log';   //private
 
-  function log(message) {       //private
+
+class Logger extends EventEmitter {           //pascal naming for classes
+  log(message) {       //don't need function key word (function inside a class - now a method)
     //send http request
-    console.log(message);
+    console.log(message);   
+
+    //raise an event                - send multiple values about an event inside an object { properties: }
+    //Emit - making a noise, produce something - signalling that an event happened
+    this.emit('messageLogged', { id: 1, url: 'http://'});    
+
   }
+}
+
+//raise:  logging (data: message)
+//const log = require('./logger');
+//log('message');
 
 //module.exports.log = log;    //add method (log) to module exports objects - empty object
-module.exports = log;      //export single function
+module.exports = Logger;      //export single function - now export Logger class
 //module.exports.endPoint = url;     // if we wanted to export
 
 //})
